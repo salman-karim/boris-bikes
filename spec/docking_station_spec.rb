8289ba21ec69fb397fc1b743bf48d78ff6bfccb5
Salman <salman.karim@hotmail.com>
2015-06-10 18:21:11 +0100

    test for DockingStation#release_bike to be_working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,4 +6,8 @@ describe DockingStation do
     expect(subject).to respond_to :release_bike
   end
 
+  it 'should release a working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
