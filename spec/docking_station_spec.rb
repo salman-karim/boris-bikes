01f6c85a2939f68ed66d26cd413b87b6ca65fb8c
Salman <salman.karim@hotmail.com>
2015-06-11 16:37:42 +0100

    added docking station capacity

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,6 +7,7 @@ describe DockingStation do
   end
 
   it 'should release a working bike' do
+    subject.dock Bike.new
     bike = subject.release_bike
     expect(bike).to be_working
   end
@@ -19,4 +20,16 @@ describe DockingStation do
       expect {subject.release_bike}.to raise_error 'No bikes available'
     end
   end
+
+  describe 'dock' do
+    it 'raises an error when full' do
+      20.times {subject.dock Bike.new}
+      expect{subject.dock Bike.new}.to raise_error 'Docking station full'
+    end
+  end
+
+
+
+
+
 end
