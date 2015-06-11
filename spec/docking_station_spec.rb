8469a3ee146c251889bb53d1458dca1d2b83ac0f
Salman <salman.karim@hotmail.com>
2015-06-11 17:27:08 +0100

    defined DockingStation#capacity method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,18 +2,31 @@ require 'docking_station'
 
 describe DockingStation do
 
-  it 'should respond to release_bike' do
-    expect(subject).to respond_to :release_bike
+    it 'should respond to release_bike' do
+      expect(subject).to respond_to :release_bike
+    end
+
+    it 'should release a working bike' do
+      subject.dock Bike.new
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
+
+
+    it { is_expected.to respond_to(:dock).with(1).argument}
+
+    it 'has a default capacity' do
+      expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+    end
+
+  describe 'capacity' do
+    it 'can set the capacity' do
+      expect(subject.capacity=(50)).to eq 50
+    end
   end
 
-  it 'should release a working bike' do
-    subject.dock Bike.new
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
 
 
-  it { is_expected.to respond_to(:dock).with(1).argument}
 
   describe 'release_bike' do
     it 'raises an error when there are no bikes available' do
@@ -23,10 +36,14 @@ describe DockingStation do
 
   describe 'dock' do
     it 'raises an error when full' do
-      20.times {subject.dock Bike.new}
+      subject.capacity.times {subject.dock Bike.new}
       expect{subject.dock Bike.new}.to raise_error 'Docking station full'
     end
   end
+
+
+
+
 
 
 
